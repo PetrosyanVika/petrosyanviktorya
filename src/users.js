29c6930809a6efd2f2ed734594738d7654b14604
Vika <viktorya[etrosyan36@gmail.com>
2024-04-24 21:27:29 +0400

    my first commit

---
+++ src/users.js
@@ -0,0 +1,17 @@
+export const users=[
+    {
+        id:1,
+        name:'John',
+        description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti laboriosam corrupti ex tempore ipsa minima rerum in, quibusdam ea facere architecto, voluptates amet impedit accusamus, animi quod esse earum voluptatibus.'
+    },
+    {
+        id:2,
+        name:'Leonid',
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti laboriosam corrupti ex tempore ipsa minima rerum in, quibusdam ea facere architecto, voluptates amet impedit accusamus, animi quod esse earum voluptatibus."
+    },
+    {
+        id:3,
+        name:'Spartak',
+        description:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti laboriosam corrupti ex tempore ipsa minima rerum in, quibusdam ea facere architecto, voluptates amet impedit accusamus, animi quod esse earum voluptatibus."
+    }
+]
