cb00cfd1749185e579259a21f961d3f70bc53f2a
Vika <viktorya[etrosyan36@gmail.com>
2024-05-23 14:47:20 +0400

    Modal Window example

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./TravelLand/App";
+// import App from "./TravelLand/App";
+import App from './Modal/App'
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
