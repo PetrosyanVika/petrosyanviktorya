f0d13991e5fb63917b1740d9113dae2b987a0637
Viktorya <viktoryapetrosyan36@gmail.com>
2024-05-27 18:17:15 +0400

    Fix error

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./TravelLand/App";
 import App from './Modal/App'
-<<<<<<< HEAD
-
-=======
-import { BrowserRouter as Router } from "react-router-dom";
->>>>>>> cb00cfd1749185e579259a21f961d3f70bc53f2a
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
